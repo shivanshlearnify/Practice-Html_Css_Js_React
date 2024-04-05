faaa5fe0e7eab4f5bccfc999368f4649ebc1b222
shivanshlearnify <kesarwani.shivansh@gmail.com>
2024-04-05 18:28:49 +0530

    learning js

--- js/toDoApp/script.js
+++ js/toDoApp/script.js
@@ -1,26 +1,31 @@
-let list = [];
+let todo = [];
 
 let req = prompt('Enter Your Request -- Add, Delete, list, quit');
 
 while (true) {
 
     if (req == 'quit') {
+        console.log('quitting app');
         break;
     }
-    if(req == 'add'){
-        let task = prompt("Enter task to add");
-        list.push(task);
-        console.log('task added');
-    } else if(req == 'list'){
-        for(let i=0; i<list.length; i++){
-            console.log("--------------------");
+    if (req == 'list') {
+        console.log("--------------------");
+        for (let i = 0; i < list.length; i++) {
             console.log(i, list[i]);
-            console.log("--------------------");
         }
-    } else if(req == 'delete'){
+        console.log("--------------------");
+    }
+     else if (req == 'add') {
+        let task = prompt("Enter task to add");
+        todo.push(task);
+        console.log('task added');
+    } 
+    else if (req == 'delete') {
         let idx = prompt("enter the index to delete");
         list.splice(idx, 1);
         console.log('task deleted');
-    } 
+    } else {
+        console.log("invalid request");
+    }
     req = prompt('Enter Your Request -- Add, Delete, list, Quit');
 }
