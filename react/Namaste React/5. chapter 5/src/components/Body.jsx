b4323cdc2cb4e887e3aa98fcc2f3d03df03f6aa5
shivanshlearnify <kesarwani.shivansh@gmail.com>
2024-06-02 23:36:19 +0530

    learning

--- react/Namaste React/5. chapter 5/src/components/Body.jsx
+++ react/Namaste React/5. chapter 5/src/components/Body.jsx
@@ -1,26 +1,59 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resListData, setResListData] = useState(resList);
+  const [resListData, setResListData] = useState([]);
+  const [search, setSearch] = useState([]);
 
   const handleOnFilter = () => {
     setResListData(
-      resList.filter((data) => {
-        return Number(data.data.avgRating) >= 4.3;
+      resListData.filter((data) => {
+        return Number(data.info.avgRating) >= 4.3;
       })
     );
   };
-  return (
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setResListData(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return resListData.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <button className="btn-Filter" onClick={handleOnFilter}>
-        Top Rated
-      </button>
+      <div className="filter">
+        <button className="btn-Filter" onClick={handleOnFilter}>
+          Top Rated
+        </button>
+        <div className="search">
+          <input
+            type="text"
+            className="text-search"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <button onClick={()=> console.log(search)}>Search</button>
+        </div>
+      </div>
       <div className="res-container">
         {resListData.map((restaurant) => {
           return (
-            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
         })}
       </div>
--- react/Namaste React/5. chapter 5/src/components/RestaurantCard.jsx
+++ react/Namaste React/5. chapter 5/src/components/RestaurantCard.jsx
@@ -5,17 +5,15 @@ const RestaurantCard = ({ resData }) => {
     name,
     cuisines,
     cloudinaryImageId,
-    minDeliveryTime,
     totalRatingsString,
     avgRating,
-  } = resData?.data;
+  } = resData?.info;
   return (
     <div className="res-card">
       <img className="res-cardImg" src={CDN_URL + cloudinaryImageId} alt={name}/>
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{totalRatingsString}</h4>
-      <h4>{minDeliveryTime} Min Delivery Time</h4>
       <h4>{avgRating} Star</h4>
     </div>
   );
