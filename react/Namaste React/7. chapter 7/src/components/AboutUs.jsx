e1293296cbc7629d6dcce06d05e57913a4bacfe8
shivanshlearnify <kesarwani.shivansh@gmail.com>
2024-06-03 22:52:23 +0530

    learning

---
+++ react/Namaste React/7. chapter 7/src/components/AboutUs.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default AboutUs;
---
+++ react/Namaste React/7. chapter 7/src/components/Body.jsx
@@ -0,0 +1,78 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  const [resListData, setResListData] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [search, setSearch] = useState([]);
+
+  const handleOnFilter = () => {
+    setFiltered(
+      filtered.filter((data) => {
+        return Number(data.info.avgRating) >= 4.5;
+      })
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setResListData(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFiltered(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  return resListData.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <button className="btn-Filter" onClick={handleOnFilter}>
+          Top Rated
+        </button>
+        <div className="search">
+          <input
+            type="text"
+            className="text-search"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filterSearch = resListData.filter((data) =>
+                data.info.name.toLowerCase().includes(search.toLowerCase())
+              );
+              setFiltered(filterSearch);
+              setSearch("");
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="res-container">
+        {filtered.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
